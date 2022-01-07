01fceea30a7e475d272047bec78bd5bb3ae2cffc
jihyunhillpark <girl9505@naver.com>
2022-01-07 17:10:03 +0900

    refactor : seprate functions into methods

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,20 +8,20 @@ public class Application {
     public static void main(String[] args) {
         // TODO 숫자 야구 게임 구현
         boolean flag = true;
-        int[] ans = new int[3];
-        int[] input = new int[3];
         while (flag) {
-            getNumberFromComputer(ans); // 1. user 가 맞춰야 할 난수를 생성 한 뒤 길이간 3인 int형 array에 보관
-            while(true){
-                getInputFromUser(input); // 2. user에게 인풋 받아오기
-                if(compareAnswerAndFinish(ans, input) == 1) { // 3. user의 input 정합성 맞추기
-                    if(!restartGame())// 4. 다시 게임을 이어나갈지 종료할 것인지 받아오기 - 2를 눌렀을 경우 false 가 return 됨.
-                        flag = false;
-                    break;
-                }
-            }
+            flag = startUserTurn();
         }
         System.out.println("게임 끝");
+    }
+
+    private static boolean startUserTurn() {
+        int[] ans = new int[3];
+        int[] input = new int[3];
+        getNumberFromComputer(ans); // 1. user 가 맞춰야 할 난수를 생성 한 뒤 길이간 3인 int형 array에 보관
+        do {
+            getInputFromUser(input); // 2. user에게 인풋 받아오기
+        } while (!compareAnswer(ans, input)); // 3 strike가 나오면 게임이 끝남
+        return restartGame();
     }
 
     static void getNumberFromComputer(int[] input) {
@@ -38,48 +38,38 @@ public class Application {
     }
 
     static void getInputFromUser(int[] input) {
-        boolean flag = true;
-        boolean[] visit = new boolean[9];
-        while(flag) {
-            flag = false;
+        boolean flag = false;
+        while(!flag) {
             System.out.print("숫자를 입력해 주세요 : ");
-            char[] in = Console.readLine().toCharArray();
-            if(in.length != 3) System.out.println("[ERROR]"); // 인풋 길이가 잘못된 경우
-            for (int i = 0; i < input.length; i++) {
-                input[i] = in[i]-'0';
-                if(input[i]  < 1 || 9 <input[i] || visit[input[i]-1]) {
-                    System.out.println("[ERROR]"); // 범위의 값을 입력하지 않거나 중복 숫자가 나온 경우
-                    flag = true;
-                    break;
-                }
-                else visit[input[i] - 1] = true;
-            }
+            flag = isDigitsValid(input);
         }
     }
 
-    private static int compareAnswerAndFinish(int[] ans, int[] input) {
-        int balls = 0;
-        int strikes = 0;
+    private static boolean compareAnswer(int[] ans, int[] input) {
+        int[] result = new int[2]; // 0 - strikes, 1 - balls
         for (int i = 0; i < input.length; i++) {
             for (int j = 0; j < ans.length; j++) {
-                if (input[i] == ans[j]) {
-                    if (i == j ) strikes++;
-                    else balls++;
-                }
+                markResult(i, input[i], j, ans[j], result);
             }
         }
-        printResult(balls, strikes);
-        if(strikes == 3) return 1;
-        return 0;
+        printResult(result);
+        return result[0] == 3;
     }
-    private static void printResult(int balls, int strikes){
-        if (strikes == 0 && balls == 0) {
+
+    private static void markResult(int idx, int input, int ansIdx, int ans, int[] result) {
+        if (input == ans) {
+            if (idx == ansIdx ) result[0]++;
+            else result[1]++;
+        }
+    }
+    private static void printResult(int[] result){
+        if (result[0] == 0 && result[1] == 0) {
             System.out.println("낫싱");
         } else {
-            if (strikes > 0) System.out.print(strikes + "스트라이크 ");
-            if (balls > 0) System.out.print(balls + "볼");
+            if (result[0]> 0) System.out.print(result[0] + "스트라이크 ");
+            if (result[1] > 0) System.out.print(result[1] + "볼");
             System.out.println();
-            if (strikes == 3) System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임종료");
+            if (result[0] == 3) System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임종료");
         }
     }
     private static boolean restartGame(){
@@ -93,5 +83,21 @@ public class Application {
             }
         }
     }
-
+    private static boolean isDigitsValid(int[] input){
+        boolean[] visit = new boolean[9];
+        char[] in = Console.readLine().toCharArray();
+        if(in.length != 3) {
+            System.out.println("[ERROR]"); // 인풋 길이가 잘못된 경우
+            return false;
+        }
+        for (int i = 0; i < input.length; i++) {
+            input[i] = in[i]-'0';
+            if(input[i]  < 1 || 9 <input[i] || visit[input[i]-1]) {
+                System.out.println("[ERROR]"); // 범위의 값을 입력하지 않거나 중복 숫자가 나온 경우
+                return false;
+            }
+            else visit[input[i] - 1] = true;
+        }
+        return true;
+    }
 }
