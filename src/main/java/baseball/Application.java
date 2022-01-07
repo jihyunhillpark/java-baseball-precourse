b604b620e1b3851b9960747919c1c0e730086102
1101730 <a1101730@1101730M01.local>
2022-01-07 14:13:48 +0900

    feat : generate random 3 digits
    generate random 3 ditgits
    - each digit shold be between 1 to 9
    - all three digits stored in Integer Array

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,15 @@
 package baseball;
+import nextstep.utils.Console.*;
+import nextstep.utils.Randoms;
+import nextstep.utils.Randoms.*;
 
 public class Application {
     public static void main(String[] args) {
         // TODO 숫자 야구 게임 구현
+        // 1. user 가 맞춰야 할 난수를 생성 한 뒤 길이간 3인 int형 array에 보관
+        int ans[] = new int[3];
+        for(int i = 0 ; i < 3 ; i++)
+            ans[i] = Randoms.pickNumberInRange(1,9);
+        // 2.
     }
 }
